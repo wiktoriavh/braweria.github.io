5e8664607b4d000f319a437cabaf08809f416f04
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-07-10 13:21:03 +0200

    click event und cursor update

--- JavaScript30/js/01-drums.js
+++ JavaScript30/js/01-drums.js
@@ -1,14 +1,26 @@
-window.addEventListener("keydown", function (e) {
+function playSoundKey(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-	if (!audio) return;
-
-
+	if (!audio) return; // mach nichts, wenn keine richtige Taste gespielt wird
+	
 	audio.currentTime = 0;
 	audio.play();
 	key.classList.add("clap");
-});
+}
+
+function playSoundClick(c) {
+	const click = c.toElement.dataset.key;
+
+	const audio = document.querySelector(`audio[data-key="${click}"]`);
+	const key = document.querySelector(`.key[data-key="${click}"]`);
+
+	if (!audio) return; // mach nichts, wenn keine richtige Taste gespielt wird
+	
+	audio.currentTime = 0;
+	audio.play();
+	key.classList.add("clap");
+}
 
 const keys = document.querySelectorAll(".key");
 
@@ -17,4 +29,6 @@ function removeTransition(e) {
 	this.classList.remove("clap");
 }
 
+window.addEventListener("keydown", playSoundKey);
+window.addEventListener("click", playSoundClick);
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
