52576731126d34fd06c89b8b280bd4292970d36e
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-07-10 18:06:57 +0200

    V1.1 BPM hinzugefügt

--- JavaScript30/js/01-drums.js
+++ JavaScript30/js/01-drums.js
@@ -2,8 +2,8 @@ function playSoundKey(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-	if (!audio) return; // mach nichts, wenn keine richtige Taste gespielt wird
-	
+	if (!audio) { return; } // mach nichts, wenn keine richtige Taste gespielt wird
+
 	audio.currentTime = 0;
 	audio.play();
 	key.classList.add("clap");
@@ -16,7 +16,7 @@ function playSoundClick(c) {
 	const key = document.querySelector(`.key[data-key="${click}"]`);
 
 	if (!audio) return; // mach nichts, wenn keine richtige Taste gespielt wird
-	
+
 	audio.currentTime = 0;
 	audio.play();
 	key.classList.add("clap");
@@ -25,9 +25,45 @@ function playSoundClick(c) {
 const keys = document.querySelectorAll(".key");
 
 function removeTransition(e) {
-	if(e.propertyName !== "transform") return;
+	if (e.propertyName !== "transform") { return; }
 	this.classList.remove("clap");
 }
+
+let rythm;
+
+window.addEventListener("click", function(b) {
+	const bpm = b.target.dataset.bpm;
+	const btn = b.target
+	const btns = document.querySelectorAll(".btn"); // NodeList
+	const tap = document.querySelector("audio.tap");
+
+	if (bpm == undefined) {
+		return;
+	}
+
+	if (b.target === document.getElementsByTagName("body")) { return; }
+
+	btns.forEach(element => element.classList.remove("active")); // entfernt die Klasse active von allen
+	btn.classList.add("active");
+	
+	clearInterval(rythm);
+	if (bpm == 00) {
+		return tap.pause();
+	}
+	
+
+	console.log(bpm);
+	
+	
+	rythm = setInterval(function() {
+		tap.currentTime = 0;
+		tap.play(); 
+	}, 60000/bpm);
+
+
+	console.log(rythm);
+
+});
 
 window.addEventListener("keydown", playSoundKey);
 window.addEventListener("click", playSoundClick);
--- JavaScript30/js/dist/01-drums.dev.js
+++ JavaScript30/js/dist/01-drums.dev.js
@@ -1,20 +1,71 @@
 "use strict";
 
-window.addEventListener("keydown", function (e) {
+function playSoundKey(e) {
   var audio = document.querySelector("audio[data-key=\"".concat(e.keyCode, "\"]"));
   var key = document.querySelector(".key[data-key=\"".concat(e.keyCode, "\"]"));
-  if (!audio) return;
+
+  if (!audio) {
+    return;
+  } // mach nichts, wenn keine richtige Taste gespielt wird
+
+
   audio.currentTime = 0;
   audio.play();
   key.classList.add("clap");
-});
+}
+
+function playSoundClick(c) {
+  var click = c.toElement.dataset.key;
+  var audio = document.querySelector("audio[data-key=\"".concat(click, "\"]"));
+  var key = document.querySelector(".key[data-key=\"".concat(click, "\"]"));
+  if (!audio) return; // mach nichts, wenn keine richtige Taste gespielt wird
+
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("clap");
+}
+
 var keys = document.querySelectorAll(".key");
 
 function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
+  if (e.propertyName !== "transform") {
+    return;
+  }
+
   this.classList.remove("clap");
 }
 
+var rythm;
+window.addEventListener("click", function (b) {
+  var bpm = b.target.dataset.bpm;
+  var btn = b.target;
+  var btns = document.querySelectorAll(".btn"); // NodeList
+
+  var tap = document.querySelector("audio.tap");
+
+  if (b.target === document.getElementsByTagName("body")) {
+    return;
+  }
+
+  btns.forEach(function (element) {
+    return element.classList.remove("active");
+  }); // entfernt die Klasse active von allen
+
+  btn.classList.add("active");
+
+  if (bpm === 0) {
+    return clearInterval(rythm);
+  }
+
+  clearInterval(rythm);
+  rythm = setInterval(function () {
+    tap.currentTime = 0;
+    tap.play();
+  }, 60000 / bpm);
+  console.log(rythm);
+});
+window.addEventListener("keydown", playSoundKey);
+window.addEventListener("click", playSoundClick);
 keys.forEach(function (key) {
   return key.addEventListener("transitionend", removeTransition);
 });
